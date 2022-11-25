583f2bbd2a8996d0f83b934c5cdb84d168346b94
neonapinto <neonapinto@gmail.com>
2022-11-25 14:34:51 -0500

    Updated Package.json

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -6,7 +6,8 @@ const Cards:FC<any> = (props:any) => {
     return (props.children.map( (item:any) =>{
         return (
             <AnimationOnScroll animateIn='animate__fadeInLeftBig'  key={item.img_src}>
-                <div className='cards-container' key={item.img_src} onClick={() => {window.open(item.link,'_blank')}}>
+                {/* <div className='cards-container' key={item.img_src} onClick={() => {window.open(item.link,'_blank')}}> */}
+                <div className='cards-container' key={item.img_src}>
                     <div className='cards-img-container'>
                         <img src={`/img/projects/${item.img_src}.jpg`} alt={item.name} className='cards-img' loading='lazy'/>
                     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register("/serviceworker.js");
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
